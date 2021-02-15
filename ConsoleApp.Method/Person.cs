286da3ac68202e1dea54a7dda81d4169150a2dc1
Ktasd Asffa <sapegina212@mail.ru>
2021-02-15 21:24:23 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp.Method/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp.Method
+{
+  class Person
+  {
+    public string SecondName { get; set; }
+    public string Name { get; set; }
+
+    public int X { get; set; }
+    public int Y { get; set; }
+
+
+    public Person(string secondName, string name)
+    {
+      SecondName = secondName;
+      Name = name;
+
+      X = 0;
+      Y = 0;
+    }
+    public string Move()
+    {
+      var rnd = new Random();
+
+      X += rnd.Next(-2, 2);
+      Y += rnd.Next(-2, 2);
+
+      return $"{Name} ({X}, {Y})";
+    }
+
+    public string Move(int x, int y)
+    {
+      X += x;
+      Y += y;
+
+      return $"{Name} ({X}, {Y})";
+    }
+  }
+}
---
+++ ConsoleApp.Method/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp.Method
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var person1 = new Person("Kuzmin", "Vladimir");
+      var person2 = new Person("Lavrentiev", "Mikhail");
+      for (int i = 0; i < 10; i++)
+      {
+        var position1 = person1.Move();
+        Console.WriteLine(position1);
+
+        var position2 = person2.Move(1, 1);
+        Console.WriteLine(position2);
+      }
+
+      Console.WriteLine($"Factorial: {Factorial(5)}");
+      Console.ReadLine();
+    }
+
+    public static int Factorial(int value)
+    {
+      if (value <= 1)
+        return 1;
+      else
+      {
+        return value * Factorial(value - 1);
+      }
+    }
+
+  }
+}
